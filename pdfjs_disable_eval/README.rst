06dacdae72ab4975c8ed68d90d53f86ae24cd3ce
n0n <martti@avalah.ee>
2024-05-23 10:37:20 +0300

    [ADD] module stub

---
+++ pdfjs_disable_eval/README.rst
@@ -0,0 +1,11 @@
+====================================
+Disable PDF.js Javascript Evaluation
+====================================
+
+
+Changelog
+=========
+
+- 14.0.0.0.0
+    - Initial commit
+
---
+++ pdfjs_disable_eval/__manifest__.py
@@ -0,0 +1,13 @@
+# noinspection PyStatementEffect
+{
+    'name': 'Disable PDF.js Javascript Evaluation',
+    'version': '0.0.0',
+    'author': 'Avatud Lahendused',
+    'license': 'Other proprietary',
+    'website': 'https://www.avalah.ee',
+    'data': [
+    ],
+    'depends': [
+    ],
+    'installable': True,
+}

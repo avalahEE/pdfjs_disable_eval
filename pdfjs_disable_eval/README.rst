cff5c4e2dfab49ed49960347b43bd343e939e9b3
Rasmus <rasmus@avalah.ee>
2024-05-24 10:16:47 +0300

    [ADD] JS override

--- pdfjs_disable_eval/README.rst
+++ pdfjs_disable_eval/README.rst
@@ -6,6 +6,6 @@ Disable PDF.js Javascript Evaluation
 Changelog
 =========
 
-- 14.0.0.0.0
+- 14.0.1.0.0
     - Initial commit
 
--- pdfjs_disable_eval/__manifest__.py
+++ pdfjs_disable_eval/__manifest__.py
@@ -1,13 +1,20 @@
 # noinspection PyStatementEffect
 {
     'name': 'Disable PDF.js Javascript Evaluation',
-    'version': '0.0.0',
+    'version': '1.0.0',
     'author': 'Avatud Lahendused',
     'license': 'Other proprietary',
     'website': 'https://www.avalah.ee',
     'data': [
+        'static/src/script.xml',
     ],
     'depends': [
+        'web',
+        'mail_enterprise',
+    ],
+    'qweb': [
+        'static/src/mail_enterprise.xml',
+        'static/src/thread.xml',
     ],
     'installable': True,
 }

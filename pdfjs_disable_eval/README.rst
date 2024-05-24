3570adad329d91fd10db13684887821440d0ea24
Rasmus <rasmus@avalah.ee>
2024-05-24 13:40:12 +0300

    [IMP] Improved JS patching and created xpath inheritance for mail_enterprise.xml and thread.xml

--- pdfjs_disable_eval/README.rst
+++ pdfjs_disable_eval/README.rst
@@ -6,6 +6,9 @@ Disable PDF.js Javascript Evaluation
 Changelog
 =========
 
+- 14.0.1.1.0
+    - Improved JS patching
+    - Created xpath inheritance for mail_enterprise.xml and thread.xml, instead of copying the whole file
 - 14.0.1.0.0
     - Initial commit
 
--- pdfjs_disable_eval/__manifest__.py
+++ pdfjs_disable_eval/__manifest__.py
@@ -1,7 +1,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'Disable PDF.js Javascript Evaluation',
-    'version': '1.0.0',
+    'version': '1.1.0',
     'author': 'Avatud Lahendused',
     'license': 'Other proprietary',
     'website': 'https://www.avalah.ee',
